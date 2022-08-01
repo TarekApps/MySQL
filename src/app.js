135ffc9ac42c3e0314ca5c262d07611bcd1d21b4
TarekApps <mizzy887x@gmail.com>
2022-08-01 23:50:25 +0100

    Completed

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ const { sequelize } = require("./db/connection");
 const { createMovie } = require("./movie/functions");
 const { readMovie } = require("./movie/functions");
 const { deleteMovie } = require("./movie/functions");
+const { updateMovie } = require("./movie/functions");
 
 const app = async (yargsObj) => {
-  await sequelize.sync({ alter: true });
+  await sequelize.sync();
   if (yargsObj.create) {
     await createMovie({ title: yargsObj.title, actor: yargsObj.actor });
     //add movie to db
@@ -13,6 +14,12 @@ const app = async (yargsObj) => {
     await readMovie({ title: yargsObj.title });
     //list movies from db
   } else if (yargsObj.update) {
+    await updateMovie({
+      title: yargsObj.title,
+      newTitle: yargsObj.newTitle,
+      actor: yargsObj.actor,
+      newActor: yargsObj.newActor,
+    });
     //update a movie from db
   } else if (yargsObj.delete) {
     await deleteMovie({ title: yargsObj.title });
--- src/movie/functions.js
+++ src/movie/functions.js
@@ -11,8 +11,28 @@ exports.createMovie = async (movieObj) => {
 
 exports.readMovie = async (movieObj) => {
   try {
-    const findMovie = await Movie.findAll(movieObj);
+    const findMovie = await Movie.findAll({
+      where: { title: movieObj.title },
+    });
     console.log(findMovie);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.updateMovie = async (movieObj) => {
+  try {
+    if (movieObj.title) {
+      await Movie.update(
+        { title: movieObj.newTitle },
+        { where: { title: movieObj.title } }
+      );
+    } else if (movieObj.actor) {
+      await Movie.update(
+        { actor: movieObj.newActor },
+        { where: { actor: movieObj.actor } }
+      );
+    }
   } catch (error) {
     console.log(error);
   }
